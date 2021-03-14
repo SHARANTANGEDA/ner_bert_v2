19f85ecdd08213914ad3cf9735ab9598536ac07e
Sai Sharan Tangeda <saisarantangeda@gmail.com>
2021-03-14 13:28:40 +0530

    Revert: Add on batch end for testing

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -30,18 +30,18 @@ class F1Metric(Callback):
         self.val_recalls.append(_val_recall)
         self.val_precisions.append(_val_precision)
         print(" — val_f1: % f — val_precision: % f — val_recall % f" % (_val_f1, _val_precision, _val_recall))
-
-    def on_batch_end(self, batch, logs={}):
-        output = self.model.predict(self.validation_data, batch_size=32).to_tuple()
-        print(output[0])
-        preds = np.asarray(output[1], dtype=np.float)
-        print(preds.shape)
-        val_predict = preds.round()
-        val_targ = self.label_data
-        _val_f1 = f1_score(val_targ, val_predict)
-        _val_recall = recall_score(val_targ, val_predict)
-        _val_precision = precision_score(val_targ, val_predict)
-        self.val_f1s.append(_val_f1)
-        self.val_recalls.append(_val_recall)
-        self.val_precisions.append(_val_precision)
-        print(" — val_f1: % f — val_precision: % f — val_recall % f" % (_val_f1, _val_precision, _val_recall))
+    #
+    # def on_batch_end(self, batch, logs={}):
+    #     output = self.model.predict(self.validation_data, batch_size=32).to_tuple()
+    #     print(output[0])
+    #     preds = np.asarray(output[1], dtype=np.float)
+    #     print(preds.shape)
+    #     val_predict = preds.round()
+    #     val_targ = self.label_data
+    #     _val_f1 = f1_score(val_targ, val_predict)
+    #     _val_recall = recall_score(val_targ, val_predict)
+    #     _val_precision = precision_score(val_targ, val_predict)
+    #     self.val_f1s.append(_val_f1)
+    #     self.val_recalls.append(_val_recall)
+    #     self.val_precisions.append(_val_precision)
+    #     print(" — val_f1: % f — val_precision: % f — val_recall % f" % (_val_f1, _val_precision, _val_recall))

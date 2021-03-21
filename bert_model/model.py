Fix Calculation of metrics

bert_model/model.py
@@ -63,8 +63,8 @@ def train_test(epochs, eval_batch_size, epsilon=1e-7, init_lr=2e-5, beta_1=0.9, 
     logging.info(str({"Loss": test_loss, "Micro F1/Accuracy": test_acc, "Macro F1": test_f1_macro}))
     
     # evaluate model with sklearn
-    predictions = model.predict(test_data, batch_size=eval_batch_size, verbose=1).logits
-    print(predictions)
+    predictions = np.argmax(model.predict(test_data, batch_size=eval_batch_size, verbose=1).logits, axis=-1)
+    print(np.shape(predictions), np.shape(test_labels))
     sk_report, macro_f1_score, micro_f1_score, macro_recall_score, macro_precision_score = calculate_pred_metrics(
         test_labels, predictions)
     
@@ -105,7 +105,6 @@ def load_saved_model_test(eval_batch_size=32, model_path="96_64"):
 
     # evaluate model with sklearn
     predictions = np.argmax(trained_model.predict(test_data, batch_size=eval_batch_size, verbose=1).logits, axis=-1)
-    print(predictions, test_labels)
     print(np.shape(predictions), np.shape(test_labels))
     sk_report, macro_f1_score, micro_f1_score, macro_recall_score, macro_precision_score = calculate_pred_metrics(
         test_labels, predictions)
Fix Calculation of metrics

metrics/metrics.py
@@ -91,7 +91,7 @@ def calculate_pred_metrics(y_true, y_pred):
     # true_f, pred_f = _prep_predictions(y_true, y_pred)
     print(y_true.shape, y_pred.shape)
     true_f, pred_f = np.reshape(y_true, (len(y_true)*c.MAX_SEQ_LENGTH,)), np.reshape(y_pred,
-                                                                                     (len(y_pred)*c.MAX_SEQ_LENGTH,))
+                                                                                     (len(y_pred),))
     return classification_report(true_f, pred_f, labels=c.LABELS), f1_score(true_f, pred_f, average='macro'), f1_score(
         true_f, pred_f, average='micro'), recall_score(true_f, pred_f, average='macro'), precision_score(true_f, pred_f,
                                                                                                          average='macro')
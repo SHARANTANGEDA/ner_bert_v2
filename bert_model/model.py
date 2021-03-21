Fix Calculation of metrics

bert_model/model.py
@@ -99,9 +99,9 @@ def load_saved_model_test(eval_batch_size=32, model_path="96_64"):
     
     test_data, test_inputs, test_labels = extract_features.retrieve_features(c.TEST_FILE, c.LABELS, c.MAX_SEQ_LENGTH,
                                                                              tokenizer, c.LABEL_ID_PKL_FILE)
-    # Test Scores
-    test_loss, test_acc, test_f1_macro = trained_model.evaluate(test_data, batch_size=eval_batch_size)
-    logging.info(str({"Loss": test_loss, "Micro F1/Accuracy": test_acc, "Macro F1": test_f1_macro}))
+    # # Test Scores
+    # test_loss, test_acc, test_f1_macro = trained_model.evaluate(test_data, batch_size=eval_batch_size)
+    # logging.info(str({"Loss": test_loss, "Micro F1/Accuracy": test_acc, "Macro F1": test_f1_macro}))
 
     # evaluate model with sklearn
     predictions = np.argmax(trained_model.predict(test_data, batch_size=eval_batch_size, verbose=1).logits, axis=-1)

ner_utils/extract_features.py
@@ -78,7 +78,8 @@ def retrieve_features(data_type, label_list, max_seq_length, tokenizer, label2id
         for idx, row in enumerate(input_ids):
             inputs.append(dict_from_input_data(row, attention_masks[idx], token_ids[idx]))
         
-        return dataset, inputs, np.reshape(label_ids, (len(label_ids)*c.MAX_SEQ_LENGTH, 1))
+        # return dataset, inputs, np.reshape(label_ids, (len(label_ids)*c.MAX_SEQ_LENGTH, 1))
+        return dataset, inputs, label_ids
 
 
 def retrieve_saved_model_features(data_type, label_list, max_seq_length, tokenizer, label2id_pkl_file):
3beb732c14f82b5da59bd2b8694400b9f0aa189e
Sai Sharan Tangeda <saisarantangeda@gmail.com>
2021-03-26 14:53:46 +0530

    Add New Dataset

---
+++ etl.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+# text = open('NER_data.csv', 'r').readlines()
+# t_f = []
+# for line in text:
+#     if line.startswith(","):
+#         t_f.append(line[1:])
+#     else:
+#         t_f.append(line)
+# file = open('NER_data.csv', 'w')
+# file.writelines(t_f)
+
+data, sentence, label = [], [], []
+df = pd.read_csv("NER_data.csv")
+couple = False
+
+for idx, row in df.iterrows():
+    sentence.append(row['Word'])
+    label.append(row['Label'])
+    if row['Word'] == "." and couple:
+        label = ' '.join([lbl for lbl in label if len(lbl) > 0])
+        sentence = ' '.join([word for word in sentence if len(word) > 0])
+        data.append((sentence, label))
+        sentence, label = [], []
+        couple = False
+    elif row['Word'] == '.' and not couple:
+        couple = True
+
+df_formatted = pd.DataFrame(data, columns=['sequence', 'entity_type'])
+df_formatted.to_csv("telugu_dataset/nsp_train.csv", header=True, index=False)
